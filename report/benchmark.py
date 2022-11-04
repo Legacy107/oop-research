5c5a111e0c31cbef2905857d5ab2766a048c876c
Legacy107 <quocmai1107@gmail.com>
2022-11-04 23:19:18 +1100

    fix: remove unused code

--- report/benchmark.py
+++ report/benchmark.py
@@ -1,8 +1,7 @@
 import requests
 import time
 
-# url = 'http://localhost:5050/Todo/Create'
-url = 'http://localhost:22152/Todolist/Create'
+url = 'http://localhost:5050/Todo/Create'
 
 # trigger lazy load
 requests.get(url)
@@ -14,25 +13,3 @@ r = requests.get(url)
 
 end = time.time()
 print(round((end-start) * 1000))
-
-
-# import requests
-# import time
-
-# url = 'http://localhost:5050/Todo/Create'
-# # url = 'http://localhost:22152/Todolist/Create'
-# sum = 0
-# n = 10
-
-# for i in range(n):
-#     start = time.time()
-
-#     r = requests.get(url)
-    
-#     # os.sleep(1)
-    
-#     end = time.time()
-#     print(round((end-start) * 1000))
-#     sum += end - start
-
-# print((sum / n) * 1000)
